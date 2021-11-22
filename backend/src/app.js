278fa2ff817b5c412edab69996fc8496acb9f91f
sheharyarIshfaq <sheharyarishfaq@gmail.com>
2021-11-22 11:24:18 +0500

    Added Basic Router files

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,16 +1,29 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
+
+const usersRouter = require("./routers/usersRouter");
+const passwordsRouter = require("./routers/passwordsRouter");
 
 const app = express();
 
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
+app.use(cors());
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+app.use("/api/users", usersRouter);
+app.use("/api/passwords", passwordsRouter);
 
-app.listen(port, () => {
-  console.log(`Password Manager app listening at http://localhost:${port}`);
-});
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@password-manager.sdsoa.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
+  )
+  .then(
+    app.listen(port, () => {
+      console.log(`Server is listening on ${port}`);
+    })
+  )
+  .catch((error) => {
+    console.log(error);
+  });
